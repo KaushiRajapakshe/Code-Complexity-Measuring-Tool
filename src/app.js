8b35fa82b4e78fcc67a9ff13bb50151d9abbf088
224193867 <tharidhip172002@gmail.com>
2024-06-04 21:33:50 +1000

    modifications for header and footer

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const { MongoClient } = require('mongodb');
-const uri = 'mongodb+srv://thari123:thari123@cluster0.01zhtfo.mongodb.net/';
+//const uri = 'mongodb+srv://thari123:thari123@cluster0.01zhtfo.mongodb.net/';
+const uri = '';
 const databaseName = 'code_complexity';
 
 const client = new MongoClient(uri, {
